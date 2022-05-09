0dbf263b2f76d2064a1cd1575cfc36b9611e0761
Adilet Soronov <adiletelf@gmail.com>
2022-05-09 23:41:10 +0700

    Add three.js scene with dog

---
+++ libs/model.js
@@ -0,0 +1,37 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+export function loadGLTFModel(
+    scene,
+    glbPath,
+    options = { receiveShadows: true, castShadow: true }
+) {
+    const { receiveShadows, castShadow } = options;
+    return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader();
+        loader.load(
+            glbPath,
+            gltf => {
+                const obj = gltf.scene;
+                obj.name = 'dog';
+                obj.position.y = 0;
+                obj.position.x = 0;
+                obj.receiveShadows = receiveShadows;
+                obj.castShadow = castShadow;
+                scene.add(obj);
+
+                obj.traverse(function (child) {
+                    if (child.isMesh) {
+                        child.castShadow = castShadow;
+                        child.receiveShadows = receiveShadows;
+                    }
+                });
+
+                resolve(obj);
+            },
+            undefined,
+            function (error) {
+                reject(error);
+            }
+        );
+    });
+}
