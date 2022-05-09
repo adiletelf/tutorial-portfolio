f3b1518b249530d2d3cfd2c284912065267006e7
Adilet Soronov <adiletelf@gmail.com>
2022-05-09 20:46:59 +0700

    Add navbar

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { Text, useColorModeValue } from '@chakra-ui/react'
-import styled from '@emotion/styled'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Text, useColorModeValue } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 const LogoBox = styled.span`
 font-weight: bold;
@@ -12,30 +12,34 @@ height: 30px;
 line-height: 20px;
 padding: 10px;
 
+img {
+    transition: 200ms ease;
+}
+
 &:hover img {
     transform: rotate(20deg);
 }
-`
+`;
 
 const Logo = () => {
-    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`;
     return (
         <Link href="/">
             <a>
                 <LogoBox>
                     <Image src={footPrintImg} width={20} height={20} alt="logo" />
+                    <Text
+                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                        fontFamily='M PLUS Rounded 1c'
+                        fontWeight='bold'
+                        ml={3}
+                    >
+                        Adilet Soronov
+                    </Text>
                 </LogoBox>
-                <Text
-                    color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                    fontFamily='M PLUS Rounded 1c'
-                    fontWeight='bold'
-                    ml={3}
-                >
-                    Adilet Soronov
-                </Text>
             </a>
         </Link>
-    )
-}
+    );
+};
 
 export default Logo;
